8165e81a5ab2cd4fc019a55ba7bb7b9b11b94057
monupanwar1 <monupanwar5655@gmail.com>
2026-07-20 13:59:39 +0530

    added login page ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import './index.css';
 import { router } from './router.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#F65F42',
+            colorLink: '#F65F42',
+          },
+        }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,88 @@
+import { LockFilled, LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Card, Checkbox, Flex, Form, Input, Layout, Space } from 'antd';
+import Logo from '../../components/Logo';
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Username</h1>
-      <h2>password</h2>
-    </div>
+    <>
+      <Layout
+        style={{ height: '100vh', display: 'grid', placeItems: 'center' }}
+      >
+        <Space orientation='vertical' size='large' align='center'>
+          <Layout.Content
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Logo />
+          </Layout.Content>
+          <Card
+            variant='borderless'
+            style={{ width: 300 }}
+            title={
+              <Space
+                style={{
+                  width: '100%',
+                  fontSize: 16,
+                  justifyContent: 'center',
+                }}
+              >
+                <LockFilled />
+                Sign in
+              </Space>
+            }
+          >
+            <Form initialValues={{ remember: true }}>
+              <Form.Item
+                name='username'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username',
+                  },
+                  {
+                    type: 'email',
+                    message: 'Email is not valid',
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} />
+              </Form.Item>
+              <Form.Item
+                name='password'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password',
+                  },
+                ]}
+              >
+                <Input prefix={<LockOutlined />} />
+              </Form.Item>
+              <Flex justify='space-between' align='baseline'>
+                <Form.Item name='remember' valuePropName='checked'>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href='' id='login-form-forget'>
+                  Forget password
+                </a>
+              </Flex>
+              <Form.Item>
+                <Button
+                  type='primary'
+                  htmlType='submit'
+                  style={{ width: '100%' }}
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
+    </>
   );
 };
 
